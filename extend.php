d463b733c35763b7bd817598e8c57adaa493b777
Sxi <130384567+Sxinar@users.noreply.github.com>
2026-02-17 12:40:14 +0300

    Update extend.php

--- extend.php
+++ extend.php
@@ -1,18 +1,18 @@
 <?php
 
 /*
- * This file is part of semih/flarum-lang-turkish.
+ * This file is part of sxinar/flarum-lang-turkish.
  *
- * Copyright (c) 2024 Semih.
+ * Copyright (c) 2026 sxinar.
  *
  * For the full copyright and license information, please view the LICENSE.md
  * file that was distributed with this source code.
  */
 
-namespace Semih\FlarumLangTurkish;
+namespace Sxinar\FlarumLangTurkish;
 
 use Flarum\Extend;
 
 return [
-    (new Extend\LanguagePack),
+    new Extend\LanguagePack(),
 ];

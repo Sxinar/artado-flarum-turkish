44f4740e1626c7c4bc5ff74a8284a6869fe099f5
Sxi <130384567+Sxinar@users.noreply.github.com>
2026-02-17 12:36:51 +0300

    Add files via upload

---
+++ extend.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+ * This file is part of semih/flarum-lang-turkish.
+ *
+ * Copyright (c) 2024 Semih.
+ *
+ * For the full copyright and license information, please view the LICENSE.md
+ * file that was distributed with this source code.
+ */
+
+namespace Semih\FlarumLangTurkish;
+
+use Flarum\Extend;
+
+return [
+    (new Extend\LanguagePack),
+];
